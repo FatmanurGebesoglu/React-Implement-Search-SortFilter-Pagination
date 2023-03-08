ce2bb9dd8421077c96e322ceb5945157bc9405be
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2023-03-08 12:02:27 +0300

    project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData]= useState([]);
+
+  useEffect(()=>{
+    loadUsersData();
+  },[]);
+  const loadUsersData = async () => {
+    return await axios.get("http://localhost:5000/users")
+    .then((response)=> setData(response.data))
+    .catch((err)=> console.log(err))
+  };
+
+  console.log("data",data);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Hello</h1>
     </div>
   );
 }
